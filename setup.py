01b070ae9cf7f7d06ceb168dbd2b2141a5011859
CGearhart <cgearhart@gmail.com>
2024-11-27 18:49:18 -0800

    Migrate deployment to github actions

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 
-from setuptools import setup
+from setuptools import setup, find_packages
 import os
 
 long_description = 'A simple chess model in pure Python'
-if os.path.exists('README.txt'):
-    long_description = open('README.txt').read()
+if os.path.exists('README.md'):
+    long_description = open('README.md', 'r').read()
 
 # https://pythonhosted.org/setuptools/setuptools.html#id7
 setup(
     name='Chessnut',
-    version='0.3.1',
-    packages=['Chessnut'],
+    version='0.0.0',  # This will be updated automatically by the GitHub Action
+    packages=find_packages(),
     author="Chris Gearhart",
     author_email="chris@gearley.com",
     description="A basic chess model to imports/export FEN & finds moves.",
