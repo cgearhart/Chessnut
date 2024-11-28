8af90b93f8a10437495c65733c25cde94b615bb7
CGearhart <cgearhart@gmail.com>
2024-11-27 19:21:19 -0800

    Add a static long description to setup

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@
 from setuptools import setup, find_packages
 import os
 
-long_description = 'A simple chess model in pure Python'
-if os.path.exists('README.md'):
-    long_description = open('README.md', 'r').read()
+long_description = '''\
+Chessnut is a simple chess board model written in Python. Chessnut is not a chess engine – it has no AI to play games, and it has no GUI. It is a simple package that can import/export games in Forsyth-Edwards Notation (FEN), generate a list of legal moves for the current board position, intelligently validate & apply moves (including en passant, castling, etc.), and keep track of the game with a history of both moves and corresponding FEN representation.
+
+Chessnut is not written for speed, it is written for simplicity (there are only two real classes, and only about 200 lines of code). By adding a custom move evaluation function, Chessnut could be used as a chess engine. The simplicity of the model lends itself well to studying the construction of a chess engine without worrying about implementing a chess model, or to easily find the set of legal moves for either player on a particular chess board for use in conjunction with another chess application.
+'''
 
 # https://pythonhosted.org/setuptools/setuptools.html#id7
 setup(
